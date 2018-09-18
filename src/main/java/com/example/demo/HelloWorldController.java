9a5c9bb194e3a4a500872a019f109f1e03c06302
Ronson Wuniardy <rwuniard@hotmail.com>
2018-09-18 17:05:58 -0400

    Adding REST services

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="hello")
+public class HelloWorldController {
+
+    @RequestMapping(value="world", method= RequestMethod.GET)
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @RequestMapping(value="print/{myString}", method=RequestMethod.GET)
+    public String print(@PathVariable String myString) {
+        return "hello " + myString;
+    }
+}
